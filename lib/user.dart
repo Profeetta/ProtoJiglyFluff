8e1610ffb81edf45da44755c4d5213b72f80bae2
Profeetta <71485903+Profeetta@users.noreply.github.com>
2021-08-17 13:09:36 +0300

    w

--- lib/user.dart
+++ lib/user.dart
@@ -1,15 +1,18 @@
 class User {
   final String name;
   final String email;
+  final String passW;
 
-  User(this.name, this.email);
+  User(this.name, this.email, this.passW);
 
   User.fromJson(Map<String, dynamic> json)
       : name = json["name"],
-        email = json["email"];
+        email = json["email"],
+        passW = json["pass_word"];
 
   Map<String, dynamic> toJson() => {
         "name": name,
         "email": email,
+        "password": passW,
       };
 }
